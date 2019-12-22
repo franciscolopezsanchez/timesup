fc2e8e7e64ca23a8e38b9fc540d3f40689dc6044
Jesús López Sánchez <lopezsanchezpower@gmail.com>
2019-12-22 13:59:24 +0100

    Adding ActionButton component

---
+++ src/components/actionButton/ActionButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ActionButton() {
+
+    return (
+        <div>
+            <button>Jugar</button>
+        </div>
+      )
+}
+
+export default ActionButton
--- src/components/gameCreation/GameCreation.js
+++ src/components/gameCreation/GameCreation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './GameCreation.css'
 import Title from '../title/Title'
 import PlayerList from '../playerList/PlayerList'
+import ActionButton from '../actionButton/ActionButton'
 
 function GameCreation() {
     return (
@@ -9,6 +10,7 @@ function GameCreation() {
             <Title/>
             <PlayerList/>
             <PlayerList/>
+            <ActionButton/>
         </div>
       );
 }
