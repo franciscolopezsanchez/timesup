a19307230e7e7428dbd8c58399596452ff71d1ea
Jesús López Sánchez <lopezsanchezpower@gmail.com>
2019-12-22 21:55:21 +0100

    Adding StatsView and empty css and test files

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,24 +2,27 @@ import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom"
 import './App.css'
 import GameCreation from '../gameCreation/GameCreation.js'
 import RoundInstructions from '../roundInstructions/RoundInstructions.js'
 import TurnView from '../turnView/TurnView.js'
+import StatsView from '../statsView/StatsView.js'
 
 function App() {
   return (
     <div className="App">
       <Router>
           <Switch>
-            <Route path="/turn">
+            <Route path="/play">
               <TurnView/>
             </Route>
             <Route path="/round">
               <RoundInstructions/>
+            </Route>
+            <Route path="/stats">
+              <StatsView/>
             </Route>
             <Route path="/">
               <GameCreation/>
