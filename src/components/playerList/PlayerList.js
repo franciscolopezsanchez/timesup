0f9b88a3c7843eb99b52e4b5786f983e398d6ced
Francisco Lopez <lordpakixu@gmail.com>
2019-12-23 12:39:18 +0100

    adding PlayerList tests

--- src/components/playerList/PlayerList.js
+++ src/components/playerList/PlayerList.js
@@ -6,12 +6,12 @@ import Player from "../player/Player"
 function PlayerList({ teamId, teamName, players, createPlayer, removePlayer }) {
 	if (!players) return null
 	return (
-		<div>
-			<h3>{teamName}</h3>
+		<div className="playerList">
+			{teamName && <h3>{teamName}</h3>}
 			<ul>
 				{players.map(player => {
 					return (
-						<li key={player.name}>
+						<li key={player.name} className="player">
 							<Player
 								name={player.name}
 								removePlayer={removePlayer}
@@ -19,7 +19,7 @@ function PlayerList({ teamId, teamName, players, createPlayer, removePlayer }) {
 						</li>
 					)
 				})}
-				<li>
+				<li className="playerInput">
 					<PlayerInput createPlayer={createPlayer} teamId={teamId} />
 				</li>
 			</ul>
