9ea8c11825a79323e9fd645736b15358f004fc8e
Jesús López Sánchez <lopezsanchezpower@gmail.com>
2019-12-29 13:24:45 +0100

    1st access redux in SettingList

---
+++ src/components/settingSelector/SettingSelector.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from "react"
+import "./SettingSelector.css"
+import { useTranslation } from "react-i18next"
+
+function SettingSelector({id, text, defaultValue, maxValue, minValue, stepValue, onSetSetting}) {
+	const { t } = useTranslation()
+	const [selectorValue, setSelectorValue] = useState(defaultValue)
+
+	useEffect(() => {
+		onSetSetting(id, selectorValue)
+	}, [])
+
+	function handleChange(event){
+		setSelectorValue(event.target.value)
+		onSetSetting(id, selectorValue)
+	}
+	return (
+		<div>
+			<p>{selectorValue + " " + t(text)}</p>
+			<input
+				type="range"
+				min={minValue}
+				max={maxValue}
+				step={stepValue}
+				value={selectorValue}
+				onChange={handleChange}
+			/>
+		</div>
+	)
+}
+
+export default SettingSelector
