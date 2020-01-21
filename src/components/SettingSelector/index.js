c45cbe2300dc0b41ae3bfb7d1219db0ff2a7b701
Francisco Lopez <lordpakixu@gmail.com>
2020-01-22 00:20:11 +0100

    renaming SettingSelector

---
+++ src/components/SettingSelector/index.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react"
+import {useTranslation} from "react-i18next"
+
+function SettingSelector({
+  id,
+  text,
+  defaultValue,
+  maxValue,
+  minValue,
+  stepValue,
+  onSetSetting,
+}) {
+  const {t} = useTranslation()
+
+  const [selectorValue, setSelectorValue] = useState(defaultValue)
+
+  function handleChange(event) {
+    setSelectorValue(event.target.value)
+    onSetSetting(id, selectorValue)
+  }
+  return (
+    <div>
+      <p>{selectorValue + " " + t(text)}</p>
+      <input
+        type="range"
+        min={minValue}
+        max={maxValue}
+        step={stepValue}
+        value={selectorValue}
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
+
+export default SettingSelector
