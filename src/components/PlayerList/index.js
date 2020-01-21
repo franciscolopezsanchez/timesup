fe6dddd6f4c214b950428b8960be68cd21a8e1ca
Francisco Lopez <lordpakixu@gmail.com>
2020-01-22 00:17:06 +0100

    playerList refactoring

--- src/components/PlayerList/index.js
+++ src/components/PlayerList/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import "./PlayerList.css"
 import PlayerInput from "../PlayerInput"
 import Player from "../Player"
 
--- src/components/PlayerList/index.spec.js
+++ src/components/PlayerList/index.spec.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {render, cleanup} from "@testing-library/react"
-import PlayerList from "./PlayerList"
+import PlayerList from "."
 
 afterEach(cleanup)
 
