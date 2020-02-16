ba49f6b45f795193876cf9d20d5499cccfbde9b6
Francisco Lopez <francisco.lopsan@gmail.com>
2020-02-17 00:23:16 +0100

    Create new `startNewRound` action

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,5 +1,10 @@
 export const CREATE_GAME = "CREATE_GAME"
+export const START_NEW_ROUND = "START_NEW_ROUND"
 
 export const createGame = () => ({
   type: CREATE_GAME,
+})
+
+export const startNewRound = () => ({
+  type: START_NEW_ROUND,
 })
