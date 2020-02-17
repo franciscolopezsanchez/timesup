ce589c44fafcdd5514d3af0578918f2423300950
Francisco Lopez <francisco.lopsan@gmail.com>
2020-02-18 00:07:49 +0100

    Fixing merge issues

--- src/components/TeamsSelector/index.js
+++ src/components/TeamsSelector/index.js
@@ -1,21 +1,20 @@
 import React from "react"
-import Team from "../Team";
-import styles from './TeamsSelector.module.css'
+import Team from "../team"
+import styles from "./TeamsSelector.module.css"
 
-function TeamsSelector({ teams = [], onAddTeam }) {
-    return (
-       <div className={styles.layout}>
-           {teams.map(({ title, players, onAddPlayer }) => <Team title={title} players={players} onAddPlayer={onAddPlayer} />)}
-           {
-               onAddTeam &&
-               <div className={styles.cardAddTeam}>
-                   <button className={styles.addTeam}>
-                       Add team
-                   </button>
-               </div>
-           }
-       </div>
-    )
+function TeamsSelector({teams = [], onAddTeam}) {
+  return (
+    <div className={styles.layout}>
+      {teams.map(({title, players, onAddPlayer}) => (
+        <Team title={title} players={players} onAddPlayer={onAddPlayer} />
+      ))}
+      {onAddTeam && (
+        <div className={styles.cardAddTeam}>
+          <button className={styles.addTeam}>Add team</button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default TeamsSelector
