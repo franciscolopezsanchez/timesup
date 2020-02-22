baf0a8f72acf94d97c5e5f46ec70b719600eed61
Paco Lopez <francisco.lopsan@gmail.com>
2020-02-23 00:46:53 +0100

    Combine `rounds` reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import players from "./player"
 import settings from "./setting"
 import game from "./game"
 import characters from "./characters"
+import rounds from "./rounds"
 
 export default combineReducers({
   players,
   settings,
   game,
   characters,
+  rounds,
 })
