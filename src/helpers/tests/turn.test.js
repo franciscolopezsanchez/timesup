4e360f5bf68d880ab4d3f542fff6158b935bc9f8
Paco Lopez <francisco.lopsan@gmail.com>
2020-02-26 00:17:07 +0100

    Add unit tests for `GetNextPlayer` helper

--- src/helpers/tests/turn.test.js
+++ src/helpers/tests/turn.test.js
@@ -2,7 +2,18 @@ import GetNextPlayer from "../turn"
 
 const players = [
   {name: "jesus", team: 0},
-  {name: "ro", team: 1},
+  {name: "ro", team: 0},
+  {name: "cris", team: 1},
+  {name: "curro", team: 1},
+]
+
+const threeTeams = [
+  {name: "jesus", team: 0},
+  {name: "ro", team: 0},
+  {name: "cris", team: 1},
+  {name: "curro", team: 1},
+  {name: "mama", team: 2},
+  {name: "papa", team: 2},
 ]
 
 describe("GetNextPlayer tests", () => {
@@ -16,9 +27,33 @@ describe("GetNextPlayer tests", () => {
     expect(player.name).toBe("jesus")
   })
 
-  test.skip("handles no argument: lastPlayerPlayed", () => {
+  test("handles teams with one player per team", () => {
     var lastPlayerPlayed = {name: "ro", team: 1}
     var player = GetNextPlayer(players, lastPlayerPlayed)
+    expect(player.name).toBe("jesus")
+  })
+
+  test("returns curro after ro", () => {
+    var lastPlayerPlayed = {name: "ro", team: 0}
+    var player = GetNextPlayer(players, lastPlayerPlayed)
+    expect(player.name).toBe("curro")
+  })
+
+  test("returns ro after cris", () => {
+    var lastPlayerPlayed = {name: "cris", team: 1}
+    var player = GetNextPlayer(players, lastPlayerPlayed)
+    expect(player.name).toBe("ro")
+  })
+
+  test("returns jesus after curro", () => {
+    var lastPlayerPlayed = {name: "curro", team: 1}
+    var player = GetNextPlayer(players, lastPlayerPlayed)
+    expect(player.name).toBe("jesus")
+  })
+
+  test("returns jesus after papa", () => {
+    var lastPlayerPlayed = {name: "papa", team: 2}
+    var player = GetNextPlayer(threeTeams, lastPlayerPlayed)
     expect(player.name).toBe("jesus")
   })
 })
