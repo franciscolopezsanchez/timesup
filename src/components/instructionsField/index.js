8ef75e470b1ece28a101c319809a5caf45d762ca
Paco Lopez <francisco.lopsan@gmail.com>
2020-02-28 00:55:04 +0100

    Consuming `secondsPerTurn` from store

--- src/components/instructionsField/index.js
+++ src/components/instructionsField/index.js
@@ -1,21 +1,25 @@
 import React from "react"
 import {connect} from "react-redux"
 
-function InstructionsField({round}) {
+import {getActualRound} from "../../reducers/game"
+import {getSecondsPerTurn} from "../../reducers/setting"
+
+function InstructionsField({round, secondsPerTurn}) {
   return (
     <div>
       <ul>
         <li>{"Ronda " + round}</li>
         <li>Explicación del personaje</li>
         <li>No se puede pasar</li>
-        <li>30 segundos por turno</li>
+        <li>{secondsPerTurn + " segundos por turno"}</li>
       </ul>
     </div>
   )
 }
 
 const mapStateToProps = (state, props) => ({
-  round: state.game.round,
+  round: getActualRound(state),
+  secondsPerTurn: getSecondsPerTurn(state),
 })
 
 const mapDispatchToProps = dispatch => ({})
