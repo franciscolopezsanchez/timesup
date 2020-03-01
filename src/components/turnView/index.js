fc551255dd0157cd749e135a732be0547621a6b0
Paco Lopez <francisco.lopsan@gmail.com>
2020-03-01 16:19:27 +0100

    Consume `rightAnswer` action

--- src/components/turnView/index.js
+++ src/components/turnView/index.js
@@ -2,7 +2,10 @@ import React from "react"
 import {connect} from "react-redux"
 
 import {getSecondsPerTurn} from "../../reducers/setting"
+import {getPlayerPlaying} from "../../reducers/game"
+
 import {selectCharacter, removeCharacter} from "../../actions/rounds"
+import {rightAnswer} from "../../actions/stats"
 
 import TurnScore from "../turnScore"
 import CharacterCard from "../characterCard"
@@ -11,27 +14,43 @@ import ActionButton from "../actionButton"
 
 import {useTranslation} from "react-i18next"
 
-function TurnView({finishPlaying, secondsPerTurn, selectCharacter, removeCharacter}) {
+function TurnView({
+  finishPlaying,
+  secondsPerTurn,
+  playerPlaying,
+  selectCharacter,
+  removeCharacter,
+  rightAnswer,
+}) {
   const {t} = useTranslation()
 
   return (
     <div>
+      <div>Jugando: {playerPlaying.name}</div>
       <TurnScore />
       <CharacterCard />
       <Timer seconds={secondsPerTurn} handler={finishPlaying} />
       <ActionButton buttonText={t("Next character")} handler={selectCharacter} />
-      <ActionButton buttonText={t("Right answer")} handler={removeCharacter} />
+      <ActionButton
+        buttonText={t("Right answer")}
+        handler={() => {
+          removeCharacter()
+          rightAnswer()
+        }}
+      />
     </div>
   )
 }
 
 const mapStateToProps = (state, props) => ({
   secondsPerTurn: getSecondsPerTurn(state),
+  playerPlaying: getPlayerPlaying(state),
 })
 
 const mapDispatchToProps = dispatch => ({
   selectCharacter: () => dispatch(selectCharacter()),
   removeCharacter: () => dispatch(removeCharacter()),
+  rightAnswer: () => dispatch(rightAnswer()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(TurnView)
