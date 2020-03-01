925235be103cab4a4199332689d16c264febf110
Paco Lopez <francisco.lopsan@gmail.com>
2020-03-01 20:37:10 +0100

    `selectLastPlayer` action

--- src/actions/turn.js
+++ src/actions/turn.js
@@ -1,6 +1,12 @@
 export const SELECT_NEXT_PLAYER = "SELECT_NEXT_PLAYER"
+export const SELECT_LAST_PLAYER_PLAYED = "LAST_PLAYER_PLAYED"
 
 export const selectNextPlayer = players => ({
   type: SELECT_NEXT_PLAYER,
   players,
+})
+
+export const selectLastPlayerPlayed = player => ({
+  type: SELECT_LAST_PLAYER_PLAYED,
+  player,
 })
