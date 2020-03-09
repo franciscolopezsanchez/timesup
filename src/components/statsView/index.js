ec8eb0e4735a51a3461acaeabc99272b649dccbe
Paco Lopez <francisco.lopsan@gmail.com>
2020-03-10 00:02:23 +0100

    Propagating `team_name` to `TeamScore` component

--- src/components/statsView/index.js
+++ src/components/statsView/index.js
@@ -1,25 +1,48 @@
 import React from "react"
+import {connect} from "react-redux"
+
 import TeamScore from "../teamScore"
 import ActionButton from "../actionButton"
 import {useParams} from "react-router"
 
+import {initiateRoundStats} from "../../actions/stats"
+import {selectNextPlayer} from "../../actions/turn"
+import {getPlayers} from "../../reducers/player"
+import {getNumberOfRounds} from "../../reducers/game"
+
 import {useTranslation} from "react-i18next"
 import {Link} from "react-router-dom"
 
-function StatsView() {
+function StatsView({players, numberOfRounds, selectNextPlayer, initiateRoundStats}) {
   const {t} = useTranslation()
   let {round} = useParams()
 
+  const onButtonClick = () => {
+    selectNextPlayer(players)
+    initiateRoundStats(numberOfRounds, players)
+  }
+
   return (
     <div>
-      <span>Stadisticas para la ronda {round} </span>
+      <span>Stadisticas ronda {round} </span>
       <TeamScore round={round} team={0} />
       <TeamScore round={round} team={1} />
       <Link to="/play">
-        <ActionButton buttonText={t("Next Round")} />
+        <ActionButton buttonText={t("Next Round")} handler={onButtonClick} />
       </Link>
     </div>
   )
 }
 
-export default StatsView
+const mapStateToProps = (state, props) => ({
+  players: getPlayers(state),
+  numberOfRounds: getNumberOfRounds(state),
+})
+
+const mapDispatchToProps = dispatch => ({
+  selectNextPlayer: players => dispatch(selectNextPlayer(players)),
+  initiateRoundStats: (numberOfRounds, players) =>
+    dispatch(initiateRoundStats(numberOfRounds, players)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(StatsView)
